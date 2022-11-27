b32d8b07630f57b671a89636a2f50919142b44cd
meganii <yuhei24@gmail.com>
2022-11-27 12:31:25 +0900

    Format

--- index.ts
+++ index.ts
@@ -7,27 +7,27 @@ const maxIndex = Math.floor(pageNum / 1000) + 1;
 
 const pages: any[] = [];
 const promises = [...Array(maxIndex)]
-.map(async (_, index) => {
-    const json = await fetch(`https://scrapbox.io/api/pages/${project}/?limit=${limitParam}&skip=${index*1000}`)
-    .then(res => res.json());
+  .map(async (_, index) => {
+    const json = await fetch(`https://scrapbox.io/api/pages/${project}/?limit=${limitParam}&skip=${index * 1000}`)
+      .then(res => res.json());
     pages.push(...json.pages);
-});
+  });
 await Promise.all(promises);
 const titles = pages.map((page) => page.title);
 
 const skip = 100;
 const detailPages: Array<Object> = [];
 // skip件づつfetchする
-for (let i =0; i < titles.length; i += skip) {
+for (let i = 0; i < titles.length; i += skip) {
   console.log(`[scrapbox-external-backup] Start fetching ${i} - ${i + skip} pages.`);
   // 一気にAPIを叩いてページ情報を取得する
-  const promises = titles.slice(i, i+skip)
+  const promises = titles.slice(i, i + skip)
     .map(async (pageTitle: string, j: number) => {
       console.log(`[page ${i + j}@scrapbox-external-backup] start fetching "/${project}/${pageTitle}"`);
       const res = await fetch(`https://scrapbox.io/api/pages/${project}/${encodeURIComponent(pageTitle)}`);
       console.log(`[page ${i + j}@scrapbox-external-backup] finish fetching "/${project}/${pageTitle}"`);
-      const {id, title, created, updated, lines} = await res.json();
-      detailPages.push({id, title, created, updated, lines});
+      const { id, title, created, updated, lines } = await res.json();
+      detailPages.push({ id, title, created, updated, lines });
     });
   await Promise.all(promises);
   console.log(`Finish fetching ${i} - ${i + skip} pages.`);
@@ -36,12 +36,11 @@ for (let i =0; i < titles.length; i += skip) {
 console.log(writeJson("./data.json", detailPages));
 
 function writeJson(path: string, data: object): string {
-    try {
-      Deno.writeTextFileSync(path, JSON.stringify(data));
-  
-      return "Written to " + path;
-    } catch (e) {
-      return e.message;
-    }
+  try {
+    Deno.writeTextFileSync(path, JSON.stringify(data));
+
+    return "Written to " + path;
+  } catch (e) {
+    return e.message;
   }
-  
+}
